d8150a4a419b56c50e7083b4877bdbace84bc09b
Samuel Hernandez Vicera <shernandezvicera@gmail.com>
2024-06-06 15:18:11 -0700

    Project Completed

---
+++ iExpense/AddView.swift
@@ -0,0 +1,58 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Samuel Hernandez Vicera on 3/25/24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self){
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new Expense")
+          
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing){
+                    Button("Save"){
+                        let item = ExpenseItem(name: name, type: type, amount: amount)
+                        expenses.items.append(item)
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .bottomBar){
+                    Button("Cancel"){ dismiss()}
+                }
+                
+            }
+
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,18 +7,95 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems){
+                items = decodedItems
+                return
+            }
+        }
     }
 }
 
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var issueName: String = "iExpense"
+//    @State private var showingAddExpense = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section("Personal Expenses") {
+                    ForEach(expenses.items.filter { $0.type == "Personal" }) { item in
+                        ExpenseRow(item: item)
+                    }
+                    .onDelete(perform: removeItems)
+                }
+
+                Section(header: Text("Business Expenses")) {
+                    ForEach(expenses.items.filter { $0.type == "Business" }) { item in
+                        ExpenseRow(item: item)
+                    }
+                    .onDelete(perform: removeItems)
+                }
+            }
+            .navigationTitle($issueName)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: AddView(expenses: expenses)){
+                        Label("Add Expense", systemImage: "plus")
+                    }
+                    }
+            }
+            
+//            .sheet(isPresented: $showingAddExpense) {
+//                AddView(expenses: expenses) no longer needed
+//            }
+           
+
+         
+        }
+
+    }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+struct ExpenseRow: View {
+    let item: ExpenseItem
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(item.name)
+
+            }
+            Spacer()
+            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                .fontWeight(item.amount >= 100 ? .bold : .none)
+                .fontDesign(item.amount > 10 ? .serif : .monospaced )
+        }
+    }
+}
 #Preview {
     ContentView()
 }
