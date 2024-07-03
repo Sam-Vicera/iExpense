2ea7b70a4a503312d937b80f8db03933f25105c2
Samuel Hernandez Vicera <shernandezvicera@gmail.com>
2024-07-03 11:56:10 -0700

    Sign Up view ui set up Need to add navigation functionality to bottom text.     - remove back navigation from top left by making it hidden.

---
+++ iExpense/Components/InputFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  InputFieldView.swift
+//  iExpense
+//
+//  Created by Samuel Hernandez Vicera on 7/2/24.
+//
+
+import SwiftUI
+
+struct InputFieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputFieldView()
+}
